de8a2bfa0d536ae675369ae42eb80f708e14c54a
maxthemillion <max.rahn@tum.de>
2020-03-20 16:11:50 +0100

    new data

--- covid19/src/main.js
+++ covid19/src/main.js
@@ -1,26 +1,21 @@
 import Vue from 'vue'
 import app from '@maxthemillion/vued3-linechart'
-import * as moment from 'moment'
+//import * as moment from 'moment'
 import * as d3 from "d3"
 
 Vue.config.productionTip = false
 
 let opts = {
-  dataURL: "./data/approval_allpolls.csv",
-  chartTitle: "How unpopular is the US president?",
-  chartSubTitle: "Share of respondents disapproving the president (in %)",
-  commentTitle: "The unwanted president",
-  comment: "Disapproval among Voters increased sharply right after his inauguration but levelled off at a stable 50-55 percent shortly after.",
-  binding:  { x: "modeldate", y: "disapprove_estimate", color: "subgroup"},
-  xAxisTitle: "Time in office",
+  dataURL: "./data/all_sel.csv",
+  chartTitle: "Share of deaths in confirmed COVID19 cases",
+  chartSubTitle: "Are countries undertesting?",
+  commentTitle: "Who's up to the challenge?",
+  comment: "While Germany and South Korea have very low numbers of deaths in confirmed cases, Italy and originally also the US did not. Assuming (almost) equal standards in health treatment across these industrialized countries, the figures may be an indicator on whether countries are undertesting.",
+  binding:  { x: "date", y: "death_rate", color: "country"},
+  xAxisTitle: "Days since 100th case",
   yAxisTitle: null,
   colorScheme: d3.schemeTableau10,
-  annotations: [
-    {x: moment('2017-05-09'), text:'Trump fires James Comey'},
-    {x: moment('2017-05-31'), text:'Trump tweets "covfefe"'},
-    {x: moment('2017-10-04'), text:'Trump throws paper towel at Puerto Ricans'},
-    {x: moment('2019-09-01'), text:'Trump fires ambassador to Ukraine'},
-  ]
+  annotations: []
 }
 
 new Vue({
