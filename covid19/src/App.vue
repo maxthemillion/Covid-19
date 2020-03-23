0a4f5cd9f87cf89a472d6046aa2213737ebe7722
maxthemillion <max.rahn@tum.de>
2020-03-23 08:55:16 +0100

    new d3 charts

--- covid19/src/App.vue
+++ covid19/src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrap">
+    <linechart v-bind:opts="opts_casesVS100"/>
+    <linechart v-bind:opts="opts_mortalityVS100" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import linechart from '@maxthemillion/vued3-linechart'
+import * as d3 from "d3"
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    linechart
+  },
+  data() {
+    return {
+      opts_mortalityVStime : {
+        dataURL: "./data/all_sel.csv",
+        chartTitle: "Are countries undertesting?",
+        chartSubTitle: "Share of deaths in confirmed COVID-19 cases",
+        commentTitle: "Vast differences in testing procedures",
+        comment:
+          "While Germany and South Korea have very low numbers of deaths in confirmed cases, Italy and originally also the US did not. Assuming (almost) equal standards in health treatment across these industrialized countries, the figures may be an indicator on whether countries are undertesting.",
+        binding: {
+          x: "date",
+          xType: "T",
+          y: "death_rate",
+          yType: "P",
+          color: "country",
+          colorType: "N"
+        },
+        format: {
+          y: d3.format(".1%"),
+          x: d3.timeFormat("%d %B")
+        },
+        tick: {
+          frequency: d3.timeWeek.every(1)
+        },
+        scale: {
+          x: d3.scaleTime(),
+          y: d3.scaleLinear(),
+          color: d3.scaleOrdinal()
+        },
+        xAxisTitle: "Days since 100th case",
+        yAxisTitle: null,
+        colorScheme: ["steelblue"],
+        colorHighlight: ["Germany", "Italy"],
+        annotations: []
+      },
+      opts_mortalityVS100 : {
+        dataURL: "./data/all_sel.csv",
+        chartTitle: "Is mortality really different across countries?",
+        chartSubTitle: "Share of deaths in confirmed COVID-19 cases",
+        commentTitle: "Vast differences in testing procedures",
+        comment:
+          "While Germany and South Korea have very low numbers of deaths in confirmed cases, Italy and originally also the US did not. Assuming (almost) equal standards in health treatment across these industrialized countries, the figures may be an indicator on whether countries are undertesting.",
+        binding: {
+          x: "days_since100",
+          xType: "Q",
+          y: "death_rate",
+          yType: "P",
+          color: "country",
+          colorType: "N"
+        },
+        format: {
+          y: d3.format(".1%")
+        },
+        xAxisTitle: "Days since 100th case",
+        yAxisTitle: null,
+        colorScheme: ["steelblue"],
+        colorHighlight: ["Germany", "Italy"],
+        annotations: []
+      },
+      opts_casesVS100:{
+        dataURL: "./data/all_sel.csv",
+        chartTitle: "What are the latest figures?",
+        chartSubTitle: "Confirmed COVID-19 cases since the 100th case in each country",
+        commentTitle: "Exponential growht",
+        comment:
+          "",
+        binding: {
+          x: "days_since100",
+          xType: "Q",
+          y: "confirmed",
+          yType: "Q",
+          color: "country",
+          colorType: "N"
+        },
+        format: {
+          y: d3.format("d")
+        },
+        xAxisTitle: "Days since 100th case",
+        yAxisTitle: null,
+        colorScheme: ["steelblue"],
+        colorHighlight: ["Germany", "Italy"],
+        annotations: []
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- covid19/src/main.js
+++ covid19/src/main.js
@@ -1,23 +1,11 @@
 import Vue from 'vue'
-import app from '@maxthemillion/vued3-linechart'
+import app from './App.vue'
 //import * as moment from 'moment'
+// eslint-disable-next-line no-unused-vars  
 import * as d3 from "d3"
 
 Vue.config.productionTip = false
 
-let opts = {
-  dataURL: "./data/all_sel.csv",
-  chartTitle: "Share of deaths in confirmed COVID19 cases",
-  chartSubTitle: "Are countries undertesting?",
-  commentTitle: "Who's up to the challenge?",
-  comment: "While Germany and South Korea have very low numbers of deaths in confirmed cases, Italy and originally also the US did not. Assuming (almost) equal standards in health treatment across these industrialized countries, the figures may be an indicator on whether countries are undertesting.",
-  binding:  { x: "date", y: "death_rate", color: "country"},
-  xAxisTitle: "Days since 100th case",
-  yAxisTitle: null,
-  colorScheme: d3.schemeTableau10,
-  annotations: []
-}
-
 new Vue({
-  render: h => h(app, {props:{ opts: opts}}),
+  render: h => h(app),
 }).$mount('#app')
