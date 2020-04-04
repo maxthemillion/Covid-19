9f7509ac2f7cc7e2152ac5176f2e18ed9246fb53
maxthemillion <max.rahn@tum.de>
2020-04-05 00:10:18 +0200

    content update, visitBar

--- covid19/src/App.vue
+++ covid19/src/App.vue
@@ -1,77 +1,63 @@
 <template>
   <div id="page-wrapper">
     <div id="content-wrapper">
-      <div><h1>Exploring COVID-19 case counts</h1></div>
+      <div><h1>What do COVID-19 case counts really tell us?</h1></div>
+      <visitbar />
       <div class="intro text-block">
         <p>
-          In the recent weeks, figures on COVID-19 cases around the world have really gone mad. Being confronted with those figures every day
-          really made me go mad as well as I only saw case counts surging and death rates increasing. I didn't understand what was going on. Frankly, I was
-          deeply worried about people and especially my young family. This is when I decided to have a more in depth look at the data myself. Structuring the information overload helps me to put
-          worries aside and focus on what is actually going on.
+          In recent weeks, figures on COVID-19 cases around the world have been soaring and with them I went mad as well. Frankly, I was
+          deeply worried about people and especially my young family. This is when I decided to have a more in depth look at the data myself. Structuring the information overload helps me to put worries aside and focus on what is actually going on.
         </p>
-
-        <p>
-          Disclaimer: This is a personal project, not a scientific summary of facts. It is a 
-          collection of my personal observations and a summary of my own learning process that I went through when dealing with the COVID-19 data.
-        </p>
-
-      
       </div>
       
       <div class="text-block">
         <p>
-          Data source for all charts is data collected and provided by
-          <a href="https://github.com/CSSEGISandData/COVID-19">Johns Hopkins University</a>. COVID-19 Visualizations
-          out there have been steadily improving.
-          <a
-            href="https://www.zeit.de/wissen/gesundheit/2020-03/coronavirus-deutschland-infektionen-faelle-verbreitung-epidemie-karte#karte"
-          >Zeit Online</a> publishes one of the most comprehensive and well structured
-          ones that I found so far. All German speakers: Have a look at it!
+          <i>Disclaimer: This is a collection of my <u>personal observations</u> that I made when dealing with the COVID-19 data and researching news coverage.
+          </i>
         </p>
 
         <p>
-          I use
-          <a href="http://vuejs.org">Vue.js</a> and
-          <a href="https://D3js.org">D3.js</a> to create interactive charts.
-          Find the code on
-          <a href="http://github.com/maxthemillion/covid-19">GitHub</a>.
-          I plan to update this as things go on. So it may be worthwile to come back here in a few days
+          Data source for all charts is data collected and provided by
+          <a href="https://github.com/CSSEGISandData/COVID-19">Johns Hopkins University</a>. 
+          I use <a href="http://vuejs.org">Vue.js</a> and <a href="https://D3js.org">D3.js</a> to create interactive charts.
+          Find the code on <a href="http://github.com/maxthemillion/covid-19">GitHub</a>. I plan to update this as things go on. So it may be worthwile to come back here in a few days
         </p>
-      
-      
+
+        <p>
+          <b>Let's dive into it by asking what everyone seems to ask themsevles at the moment...</b>
+        </p>
+
       </div>
       
       <linechart v-if="loaded" v-bind="opts_casesVS100" />
 
       <div class="text-block">
         <p>
-          This chart plots case counts with a twist: I normed all curves to the day where each country had their confirmed 100th case. It is 
-          clear that many countries were not particularly successful in discovering cases early on. However, it makes the nations' paths 
-          in this uphill fight somewhat comparable across time.
+          I plot case counts with a twist widely adopted by COVID-19 visualizations: All curves were normed to the day where each country had their confirmed 100th case. It is clear that many countries were not particularly successful in discovering cases early on. However, it makes the nations' paths in this uphill fight somewhat comparable across time.
+           publishes one of the most comprehensive and well structured
+          ones that I found so far. All German speakers: Have a look at it!
         </p> 
           
         <p>
           Plotting case counts on a linear y-axis scale chart shows what everyone is talking about: Exponential growth. In recent weeks, 
-          transforming the y-axis to a log scale has become increasingly popular, as the trajectory becomes linear and more pleasant to the
-          human eye. What I particularly like about the standard linear version is that - concerning case counts - you can easily spot Germany being on the same path as
-          Italy. The difference is, that Italy is few weeks ahead.
+          transforming the y-axis to a log scale has become increasingly popular, as the trajectory becomes linear and more pleasant to the human eye. Also my favourite COVID-19 report uses that scale. What I dislike about the log-transformed version is that it becomes much harder to spot huge differences in case counts. If you compare the linear display of US and Italy counts above to the log-display that eg. <a href="https://www.zeit.de/wissen/gesundheit/2020-03/coronavirus-deutschland-infektionen-faelle-verbreitung-epidemie-karte#karte">Zeit Online</a> publishes, you'll have a much harder time figuring out that the US has already more than twice as much confirmed cases (as of 04. Apr 2020).
         </p>
 
         <p>
-          A few countries, such as Spain and the US, presumably have higher growth rates. From what I read about testing in these countries, I would
-          assume reasons for those higher growth rates can partly be seen in a delay in testing. Take the US for example: Testing kits were not available,
-          officials denied necessity to test on a larger scale. With New York becoming one of the hotspots of COVID-19 in the western world, 
-          efforts have changed dramatically. The US tested much more than before and is uncovering many cases that previously went undetected. We will
-          thus see smaller growth rates soon as "old cases" are then covered and only new cases will add to the curve. 
-          However, this does not mean that efforts to contain the spread are already working.
+          Have a look at the chart above again: I highlight Italy and Germany because they seem to follow the same trajectory. Other countries, such as Spain and the US, presumably have higher growth rates. In other countries such as the UK, France and particularly South Korea, case counts increase. But can we conclude that the pandemic spreads with different speeds in those countries?
         </p>
 
         <p>
-          When viewing COVID-19 case counts, we need to keep one important thing in mind: There is no standard procedure to count infections. 
-          In fact, countries such as China have changed their criteria for counting someone as infected multiple times. Other countries
-          have not changed their procedures officially but testing capacity are limited and more cases may go undetected the more people are 
-          infected. To assess severity of the spread across countries, we should thus ask another question that may be more robust to 
-          differences in testing procedures:
+          When looking at COVID-19 case counts, we need to keep one important thing in mind: There is no standard procedure to count infections. In fact, countries such as China have changed their criteria for counting someone as infected multiple times. Other countries have not changed their procedures officially but testing capacity are limited and more cases go undetected the more people are infected.
+        </p>
+
+        <p>
+          I argue that <span class='text-highlight'>case counts are by no means a measue that should be compared across countries</span> if you intend to view the speed with which the pandemic is expanding. At best, case counts may give you a hint how good countries are in uncovering infections.
+        </p>
+
+        <p>  
+          To compare the severity and the speed of the spread across countries, the number of COVID-19 deaths may be a more reasonable figure to look at. For non-medical naives such as me, the chance for deaths from COVID-19 to go undetected seems to be much lower than missing out on infections. Therefore I expect that deaths mirror the severity of the outbreak across countries much more accurate than the potentially highly biased 
+          infection counts.
         </p>
 
       </div>
@@ -81,9 +67,7 @@
 
       <div class="text-block">
         <p>
-          For non-medical naives such as me, the chance for deaths from COVID-19 to go undetected seems to be much lower than missing out on infections.
-          Therefore I expect that deaths mirror the severity of the outbreak across countries much more accurate than the potentially highly biased 
-          infection counts. In fact, the figures paint a very different picture of Germany's and Italy's situation. The trajectories do not match. 
+          In fact, the figures paint a very different picture of Germany's and Italy's situation. The trajectories do not match. 
           Why is that?
 
         </p>
@@ -100,12 +84,14 @@
 import linechart from "@bit/maxthemillion.d3charts.c-line";
 //import selector from "./components/Select.vue";
 import { loadData } from "@bit/maxthemillion.d3charts.utils";
+import visitbar from './components/UI/VisitBar/VisitBar.vue'
 import * as d3 from "d3";
 
 export default {
   name: "App",
   components: {
-    linechart
+    linechart,
+    visitbar
   },
   data() {
     return {
@@ -248,9 +234,8 @@ export default {
   max-width: 50rem;
 }
 
-a,
-a:visited {
-  color: #0e84ff;
-  text-decoration: none;
+.text-highlight{
+  background-color:#ffeb3d;
 }
+
 </style>
